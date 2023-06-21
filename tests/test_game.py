a4f9ccea4208548ba17b55ca855b59384a9a22e6
Moein Arabi <moein.mo81@gmail.com>
2023-06-21 21:32:55 +0330

    Add unit test

---
+++ tests/test_game.py
@@ -0,0 +1,19 @@
+import unittest
+from connect_4.game import Game, HumanAgent
+
+
+class MyTestCase(unittest.TestCase):
+    def test_board1(self):
+        game = Game(HumanAgent('p1', 'red'), HumanAgent('p2', 'yellow'))
+        for s in game.board.slots:
+            self.assertEqual(len(s), 0)
+        game.drop_disc(1)
+        self.assertEqual(1, len(game.board.slots[1]))
+        game.drop_disc(1)
+        self.assertEqual(2, len(game.board.slots[1]))
+        self.assertEqual('red', game.board.slots[1].holes[0].color)
+        self.assertEqual('yellow', game.board.slots[1].holes[1].color)
+
+
+if __name__ == '__main__':
+    unittest.main()
