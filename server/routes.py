e3dfe5e929642c1d32c31a6a6f3a20c946f6f6e1
Moein Arabi <moein.mo81@gmail.com>
2023-06-22 12:14:26 +0330

    Add game endpoint

--- server/routes.py
+++ server/routes.py
@@ -1,6 +1,12 @@
 from server import app
+from flask import render_template
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
