9b67d5d45608860ea90655ec4fd05b6f52120301
Moein Arabi <moein.mo81@gmail.com>
2023-06-24 13:33:32 +0330

    Implement feature 1 and 2 heuristic level

--- connect_4/ai/adversarial.py
+++ connect_4/ai/adversarial.py
@@ -2,15 +2,13 @@ from connect_4.game import Board
 import math
 
 
-def heuristic(board: Board):
-    if count_connect_4(board):
+def heuristic(board: Board, max_color: str):
+    if is_connect_4(board, max_color) or count_connect_3_horizontal_two_adjacent(board, max_color):
         return math.inf
-    if count_connect_3_two_adjacent(board) > 0:
-        return math.inf
-    return count_connect_3_one_adjacent(board) * 9
+    return (count_connect_3_horizontal_one_adjacent(board, max_color) + count_connect_3_vertical(board, max_color)) * 9
 
 
-def count_connect_4(board: Board, color: str) -> bool:
+def is_connect_4(board: Board, color: str) -> bool:
     # Vertical connect
     for slot in board:
         count = 0
@@ -36,12 +34,84 @@ def count_connect_4(board: Board, color: str) -> bool:
     return False
 
 
-def count_connect_3_two_adjacent(board: Board) -> int:
-    pass
+def count_connect_3_vertical(board: Board, color: str) -> int:
+    sum_of_connects = 0
+    # Vertical connect
+    for slot in board:
+        count = 0
+        for disc in slot:
+            if disc.color == color:
+                count += 1
+            else:
+                count = 0
+        if count >= 3 and slot[-1] is None:
+            sum_of_connects += 1
+    return sum_of_connects
 
 
-def count_connect_3_one_adjacent(board: Board) -> int:
-    pass
+def count_connect_3_horizontal_two_adjacent(board: Board, color: str) -> bool:
+    # Horizontal connect. Holes should be like this: _ * * * _
+    for i in range(board.max_depth):
+        count = 0
+        for slot in board:
+            # Check if the bottom of empty hole is empty or not
+            if i == 0:
+                if (slot[i] is None and count == 0) or \
+                        (slot[i] is None and count == 4) or \
+                            (slot[i].color == color and count >= 1):
+                    count += 1
+                else:
+                    count = 0
+            else:
+                if (slot[i - 1] is not None and slot[i] is None and count == 0) or \
+                        (slot[i - 1] is not None and slot[i] is None and count == 4) or \
+                            (slot[i].color == color and count >= 1):
+                    count += 1
+                else:
+                    count = 0
+            if count == 5:
+               return True
+    return False
+
+
+def count_connect_3_horizontal_one_adjacent(board: Board, color: str) -> int:
+    sum_of_connects = 0
+    # Horizontal connect. Holes should be like this: * * * _ or _ * * *
+    for i in range(board.max_depth):
+        count = 0
+        # * * * _
+        for slot in board:
+            # Check if the bottom of empty hole is empty or not
+            if i == 0:
+                if (slot[i] is None and count == 3) or (slot[i].color == color):
+                    count += 1
+                else:
+                    count = 0
+            else:
+                if (slot[i - 1] is not None slot[i] is None and count == 3) or (slot[i].color == color):
+                    count += 1
+                else:
+                    count = 0
+            if count == 4:
+                sum_of_connects += 1
+                count = 0
+        # _ * * *
+        for slot in board:
+            # Check if the bottom of empty hole is empty or not
+            if i == 0:
+                if (slot[i] is None and count == 0) or (slot[i].color == color and count >= 1):
+                    count += 1
+                else:
+                    count = 0
+            else:
+                if (slot[i - 1] is not None and slot[i] is None and count == 0) or (slot[i].color == color and count >= 1):
+                    count += 1
+                else:
+                    count = 0
+            if count == 4:
+                sum_of_connects += 1
+                count = 0
+    return sum_of_connects
 
 
 # This function generates available actions for a state
