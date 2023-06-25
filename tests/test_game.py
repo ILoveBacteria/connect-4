9897e55779a893e35d0bbf38baf14c06f7a3b67b
Moein Arabi <moein.mo81@gmail.com>
2023-06-25 11:36:05 +0330

    Fix tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,53 +1,43 @@
 import unittest
-from connect_4.game import Game, HumanAgent
+from connect_4.game import Game
+from connect_4.player import HumanAgent
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.player1 = HumanAgent('p1', 'red')
+        self.player2 = HumanAgent('p2', 'yellow')
+        self.game = Game(self.player1, self.player2)
+    
     def test_board1(self):
-        game = Game(HumanAgent('p1', 'red'), HumanAgent('p2', 'yellow'))
-        for s in game.board.slots:
-            self.assertEqual(len(s), 0)
-        game.drop_disc(1)
-        self.assertEqual(1, len(game.board.slots[1]))
-        game.drop_disc(1)
-        self.assertEqual(2, len(game.board.slots[1]))
-        self.assertEqual('red', game.board.slots[1].holes[0].color)
-        self.assertEqual('yellow', game.board.slots[1].holes[1].color)
+        for slot in self.game.board:
+            self.assertEqual(len(slot), 0)
+        self.game.drop_disc(1)
+        self.assertEqual(1, len(self.game.board[1]))
+        self.game.drop_disc(1)
+        self.assertEqual(2, len(self.game.board[1]))
+        self.assertEqual('red', self.game.board[1][0].color)
+        self.assertEqual('yellow', self.game.board[1][1].color)
 
     def test_board2(self):
-        game = Game(HumanAgent('p1', 'red'), HumanAgent('p2', 'yellow'), depth=3)
-        game.drop_disc(1)
-        game.drop_disc(1)
-        game.drop_disc(1)
-        self.assertRaises(IndexError, game.drop_disc, 1)
+        self.game.drop_disc([1] * 6)
+        self.assertRaises(IndexError, self.game.drop_disc, 1)
 
     def test_win_vertical1(self):
-        game = Game(HumanAgent('p1', 'red'), HumanAgent('p2', 'yellow'))
         for i in range(6):
-            self.assertFalse(game.drop_disc(i % 2))
-        self.assertTrue(game.drop_disc(0))
+            self.game.drop_disc(i % 2)
+            self.assertIsNone(self.game.win())
+        self.game.drop_disc(0)
+        self.assertEqual(self.player1, self.game.win())
 
     def test_win_vertical2(self):
-        game = Game(HumanAgent('p1', 'red'), HumanAgent('p2', 'yellow'), depth=10)
-        self.assertFalse(game.drop_disc(0))  # winner
-        self.assertFalse(game.drop_disc(1))
-        self.assertFalse(game.drop_disc(0))
-        self.assertFalse(game.drop_disc(0))
-        for i in range(5):
-            self.assertFalse(game.drop_disc(i % 2))
-        self.assertFalse(game.drop_disc(2))
-        self.assertTrue(game.drop_disc(0))
+        self.game.drop_disc([0, 0] + [0, 2] * 3)
+        self.game.drop_disc(0)
+        self.assertEqual(self.player1, self.game.win())
 
     def test_win_horizontal(self):
-        game = Game(HumanAgent('p1', 'red'), HumanAgent('p2', 'yellow'))
-        self.assertFalse(game.drop_disc(0))  # winner
-        self.assertFalse(game.drop_disc(1))
-        self.assertFalse(game.drop_disc(2))  # winner
-        for i in range(3, 5):
-            self.assertFalse(game.drop_disc(i % 3))
-            self.assertFalse(game.drop_disc(i))  # winner
-        self.assertFalse(game.drop_disc(0))
-        self.assertTrue(game.drop_disc(5))
+        self.game.drop_disc([0, 1, 2, 0, 3, 1, 4, 0, 5])
+        self.assertEqual(self.player1, self.game.win())
 
 
 if __name__ == '__main__':
