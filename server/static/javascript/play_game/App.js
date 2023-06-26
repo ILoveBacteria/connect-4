135e4ba19e01c21e75d9bd90f9800d3be3c29f92
Moein Arabi <moein.mo81@gmail.com>
2023-06-26 11:52:14 +0330

    Add getting game info

--- server/static/javascript/play_game/App.js
+++ server/static/javascript/play_game/App.js
@@ -6,12 +6,23 @@ import {Board} from './Board'
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {mode: null}
+        this.get_game_info()
+    }
+
+    get_game_info = async () => {
+        let response = await fetch(`/api/game_info`)
+        if (!response.ok) {
+            throw 'The response code is not OK!'
+        }
+        let data = await response.json()
+        this.setState({mode: data.mode})
     }
 
     render() {
         return (
             <div>
-                <Board/>
+                <Board mode={this.state.mode}/>
             </div>
         )
     }
