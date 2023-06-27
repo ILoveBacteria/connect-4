40886d0e9981fc0e22e722bd1095ac0fd6c9fb56
Moein Arabi <moein.mo81@gmail.com>
2023-06-27 18:34:18 +0330

    Improve the hole highlight style

--- server/static/javascript/play_game/Hole.js
+++ server/static/javascript/play_game/Hole.js
@@ -9,10 +9,11 @@ export class Hole extends React.Component {
     render() {
         let style = {
             'backgroundColor': this.props.color,
-            'transition': 'background-color 0.2s ease, border 0.2s ease'
+            'transition': 'background-color 0.2s ease, border 0.2s ease, box-shadow 0.2s ease'
         }
         if (this.props.highlight) {
             style['border'] = '2px solid greenyellow';
+            style['boxShadow'] = 'rgba(0, 0, 0, 0.3) 0 20px 36px -20px inset, greenyellow 0 0 10px 0px'
         }
 
         return (
