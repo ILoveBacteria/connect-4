c7013cfc3c6b513b54557cf3661226a916717104
Moein Arabi <moein.mo81@gmail.com>
2023-06-28 15:28:15 +0330

    Improve turn arrow style

--- server/static/javascript/play_game/PlayerCard.js
+++ server/static/javascript/play_game/PlayerCard.js
@@ -11,9 +11,11 @@ export function PlayerCard(props) {
 
     return (
         <div className="player-card" style={style}>
-            <span>{icon}</span>
-            {props.player.name}
-            <span>{props.turn && arrow_icon}</span>
+            <span>
+                <span className="agent-icon">{icon}</span>
+                <span>{props.player.name}</span>
+            </span>
+            <span id="arrow-icon">{props.turn && arrow_icon}</span>
         </div>
     );
 }
@@ -39,10 +41,10 @@ let ai_icon = (
 );
 
 let arrow_icon = (
-    <svg xmlns="http://www.w3.org/2000/svg" width={SIZE} height={SIZE} fill="currentColor" className="bi bi-arrow-left"
-         viewBox="0 0 16 16">
-        <path fill-rule="evenodd"
-              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" width={SIZE} height={SIZE} fill="currentColor"
+         className="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+        <path
+            d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
     </svg>
 );
 
