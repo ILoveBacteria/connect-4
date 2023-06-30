d7803236db30cc9204355d13f731241bd7551d27
Moein Arabi <moein.mo81@gmail.com>
2023-07-01 00:06:27 +0330

    Refactor code

--- connect_4/ai/reinforcement_learning/__init__.py
+++ connect_4/ai/reinforcement_learning/__init__.py
@@ -5,4 +5,4 @@ import os
 if os.path.exists('q_table.csv'):
     q_table = pd.read_csv('q_table.csv')
 else:
-    q_table = pd.DataFrame(columns=['state', 'action', 'disc', 'quality'])
+    q_table = pd.DataFrame(columns=['state', 'action', 'color', 'quality'])
--- connect_4/ai/reinforcement_learning/q_learning.py
+++ connect_4/ai/reinforcement_learning/q_learning.py
@@ -1,9 +1,39 @@
-from connect_4.ai.adversarial import action_space
 from connect_4.game import Board, Game, AIAgent
+from connect_4.ai.adversarial import action_space
 from . import q_table
 
 
-def simplify_board(board: Board) -> (list, str | None):
+class QLearningAgent(AIAgent):
+    def __init__(self, color: str):
+        super().__init__(color)
+        self.state_action_stack = []
+
+    def drop_disc(self, slot: int):
+        state = simplify_board(self.board, self.color)
+        action = pick_best_action(self.board, state)
+        self.state_action_stack.append((state.board, action, state.color_number))
+        return super().drop_disc(action)
+
+    def win(self) -> None:
+        calculate_new_q_value(1000, self.state_action_stack)
+
+    def lose(self) -> None:
+        calculate_new_q_value(-1000, self.state_action_stack)
+
+    def draw(self) -> None:
+        calculate_new_q_value(0, self.state_action_stack)
+
+
+class State:
+    def __init__(self, board: list, color_number: int):
+        self.board = board
+        self.color_number = color_number
+
+    def __repr__(self):
+        return str(self.board)
+
+
+def simplify_board(board: Board, color: str) -> State:
     simple = list(map(list, board))
     zero_color = None
     for slot in simple:
@@ -13,12 +43,12 @@ def simplify_board(board: Board) -> (list, str | None):
                 slot[j] = 0
             else:
                 slot[j] = 1
-    return simple, zero_color
+    color_number = 0 if zero_color is None or color == zero_color else 1
+    return State(simple, color_number)
 
 
-def pick_best_action(board: Board, turn: int) -> int:
-    state = simplify_board(board)
-    max_q = q_table[(q_table['state'] == state) & (q_table['disc'] == turn)].sort_values(by='quality', ascending=False)
+def pick_best_action(board: Board, state: State) -> int:
+    max_q = q_table[(q_table['state'].apply(lambda x: x == state)) & (q_table['color'] == state.color_number)].sort_values(by='quality', ascending=False)
     if len(max_q) == 0:
         return action_space(board)[0]
     if max_q.at[0, 'quality'] >= 0:
@@ -30,51 +60,38 @@ def pick_best_action(board: Board, turn: int) -> int:
 
 
 def calculate_new_q_value(reward: int, sas: list) -> None:
-    future_q = 0
+    discount_factor = 0.5
+    learn_factor = 0.5
+    future_q = 0  # TODO: calculate future_q
     for i in range(len(sas) - 1, -1, -1):
-        state, action, disc = sas[i]
-        old_q = q_table[(q_table['state'].apply(lambda x: x == state)) & (q_table['action'] == action) & (q_table['disc'] == disc)]
-        if len(old_q) > 0:
-            old_q = old_q.at[0, 'quality']
-        else:
-            old_q = 0
-            q_table.loc[len(q_table.index)] = [state, action, disc, old_q]
-
+        state, action, color = sas[i]
+        old_q = q_table[(q_table['state'].apply(lambda x: x == state)) & (q_table['action'] == action) & (q_table['color'] == color)]
+        old_q = old_q.at[0, 'quality'] if (exist := len(old_q) > 0) else 0
         new_q = (1 - learn_factor) * old_q + learn_factor * (reward + discount_factor * future_q)
-        q_table.loc[(q_table['state'].apply(lambda x: x == state)) & (q_table['action'] == action) & (q_table['disc'] == disc), 'quality'] = new_q
+        if exist:
+            q_table.loc[(q_table['state'].apply(lambda x: x == state)) & (q_table['action'] == action) & (
+                        q_table['color'] == color), 'quality'] = new_q
+        else:
+            q_table.loc[len(q_table.index)] = [state, action, color, new_q]
         future_q = new_q
+        reward = 0
 
 
 def train(count_games: int):
     for _ in range(count_games):
-        agent1 = AIAgent('0')
-        agent2 = AIAgent('1')
+        agent1 = QLearningAgent('0')
+        agent2 = QLearningAgent('1')
         game = Game(agent1, agent2)
-        while True:
-            simplified_board, zero = simplify_board(game.board)
-            zero = agent1.color if zero is None else zero
-            one_color = agent1.color if agent2.color == zero else agent2.color
-            map_color = {zero: 0, one_color: 1}
-            disc_color = map_color[game.players[game.turn].color]
-            action = pick_best_action(game.board, disc_color)
-            state_action_stack[game.turn].append((simplified_board, action, disc_color))
-            game.drop_disc(action)
-            winner = game.win()
-            if winner is None:
-                continue
-            if game.draw():
-                calculate_new_q_value(0, state_action_stack[0])
-                calculate_new_q_value(0, state_action_stack[1])
-            elif winner == agent1:
-                calculate_new_q_value(1000, state_action_stack[0])
-                calculate_new_q_value(-1000, state_action_stack[1])
-            else:
-                calculate_new_q_value(-1000, state_action_stack[0])
-                calculate_new_q_value(1000, state_action_stack[1])
-            break
-    q_table.to_csv('q_table.csv')
+        while game.win() is None and not game.draw():
+            game.drop_disc(-1)
 
-
-state_action_stack = [[], []]
-discount_factor = 0.5
-learn_factor = 0.5
+        if game.draw():
+            agent1.draw()
+            agent2.draw()
+        elif game.win() == agent1:
+            agent1.win()
+            agent2.lose()
+        else:
+            agent1.lose()
+            agent2.win()
+    q_table.to_csv('q_table.csv', index=False)
